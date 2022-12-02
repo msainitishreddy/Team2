423c26e510538e33e1c7182c90cb88c261316970
Sai Nitish Reddy <sainitish645@gmail.com>
2022-12-02 21:09:06 +0000

    added math

---
+++ Math.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Math {
+    /*
+    Developer : Sai Nitish Kumar Reddy Maddela
+    Uni ID : 220316811
+    Function : Addition
+     */
+    public static int add(int x,int y){
+        int result = x + y;
+        return result;
+    }
+
+    public static void main(String[] args){
+        System.out.println("Enter two numbers:");
+        Scanner kin = new Scanner(System.in);
+        int a = kin.nextInt();
+        int b = kin.nextInt();
+        System.out.println("Addition: "+a+"+"+b+" = "+add(a,b));
+
+    }
+}
