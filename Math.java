76c0ac716aa26205936738de6b8593a77cfd679c
Sai Nitish Reddy <sainitish645@gmail.com>
2022-12-03 11:08:17 +0000

    Added max function

--- Math.java
+++ Math.java
@@ -14,6 +14,25 @@ public class Math {
         int result = x + y;
         return result;
     }
+    /*
+    #################Team - M2#################
+    ###########################################
+    Developer : Sai Nitish Kumar Reddy Maddela
+    Uni ID : 220316811
+    Function : Addition operation
+    ###########################################
+    ###########################################
+     */
+    public static int max(int x,int y){
+        int maximum;
+        if (x>y){
+            maximum = x;
+        }
+        else{
+            maximum = y;
+        }
+        return maximum;
+    }
 
     public static void main(String[] args){
         System.out.println("Enter two numbers:");
@@ -21,6 +40,7 @@ public class Math {
         int a = kin.nextInt();
         int b = kin.nextInt();
         System.out.println("Addition: "+a+"+"+b+" = "+add(a,b));
+        System.out.println("Maximum of "+a+" , "+b+" : "+max(a, b));
 
     }
 }
