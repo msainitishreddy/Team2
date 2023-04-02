0dab04c622d5da6dc13f818f7d14bf395d1a656f
msainitishreddy <119310939+msainitishreddy@users.noreply.github.com>
2023-04-02 23:19:33 +0100

    Delete prorunner directory
    Duplicate folder of proRunner

--- prorunner/src/main/java/com/ecommerce/prorunner/service/OrderService.java
+++
@@ -1,8 +0,0 @@
-package com.ecommerce.prorunner.service;
-
-import com.ecommerce.prorunner.entity.OrderEntity;
-
-public interface OrderService {
-    OrderEntity createOrder(OrderEntity orderEntity);
-    void cancelOrDeleteOrder(Long Id);
-}
